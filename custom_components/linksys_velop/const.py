239bafe91811fc2fd0fa7fbb3e11266d6df73f3e
Bob Ehlers <Bob@conexo.casa>
2026-06-15 19:48:53 -0400

    Add custom_components/linksys_velop/const.py

---
+++ custom_components/linksys_velop/const.py
@@ -0,0 +1,16 @@
+"""Constants for Linksys Velop integration."""
+
+DOMAIN = "linksys_velop"
+
+CONF_HOST = "host"
+CONF_PASSWORD = "password"
+CONF_PORT = "port"
+
+DEFAULT_PORT = 80
+DEFAULT_SCAN_INTERVAL = 30
+
+ATTR_CONNECTED_DEVICES = "connected_devices"
+ATTR_IP_ADDRESS = "ip_address"
+ATTR_MAC_ADDRESS = "mac_address"
+ATTR_MODEL = "model"
+ATTR_FIRMWARE = "firmware_version"
